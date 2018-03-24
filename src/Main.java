bea80ff7fb9cdba908af40ecd08c08a079433ffc
danil <danilaeremenko@mail.ru>
2018-03-24 03:20:19 +0300

    Где донаты?

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 
 public class Main {
-    private static String input = "WhaBoutThisMauriceBitch/BinaryToDecimal/FalseEdsac.txt";
-    private static String output = "WhaBoutThisMauriceBitch/BinaryToDecimal/RightEdsac.txt";
+    private static String input = "BinaryToDecimal/FalseEdsac.txt";
+    private static String output = "BinaryToDecimal/RightEdsac.txt";
     private static boolean withComments = false;
 
     public static void main(String[] args) {
