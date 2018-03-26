958b6d5da35c483f7969357fa70abb55fc84d54a
danil <danilaeremenko@mail.ru>
2018-03-26 14:37:46 +0300

    УБИЛ.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    private static String input = "RightBinaryToDecimal/BeforeLinkingEdsac.txt";
-    private static String output = "RightBinaryToDecimal/AfterLinkingEdsac.txt";
-
+//    private static String input = "RightBinaryToDecimal/BeforeLinkingEdsac.txt";
+//    private static String output = "RightBinaryToDecimal/AfterLinkingEdsac.txt";
+    private static String input = "RightBinaryToDecimal/NotDebugBeforeLinkingEdsac.txt";
+    private static String output = "RightBinaryToDecimal/NotDebugAfterLinkingEdsac.txt";
     private static boolean withComments = true;
 
     public static void main(String[] args) {
