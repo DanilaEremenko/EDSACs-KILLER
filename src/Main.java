1c68a1b2b247a0c96b52924e9b709511f04a4cf1
Danila Eremenko <danilaeremenko@mail.ru>
2018-03-29 07:24:01 +0300

    Финальная версия.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     private static String input = "RightBinaryToDecimal/BeforeLinkingEdsac.txt";
     private static String output = "RightBinaryToDecimal/AfterLinkingEdsac.txt";
-    private static String numberedInput="RightBinaryToDecimal/NumberedBeforeLinkingEdsac.txt";
-    private static String numberedOutput="RightBinaryToDecimal/NumberedAfterLinkingEdsac.txt";
-//    private static String input = "RightBinaryToDecimal/NotDebugBeforeLinkingEdsac.txt";
-//    private static String output = "RightBinaryToDecimal/NotDebugAfterLinkingEdsac.txt";
     private static boolean withComments = true;
 
     public static void main(String[] args) {
         EdsacLinker edsacLinker = new EdsacLinker("link_", "_link");
-        edsacLinker.deleteUnusedComments(input,input);
         edsacLinker.makeLinkingFromFile(input, output, withComments);
-        edsacLinker.printNumberOfLines(input,numberedInput);
-        edsacLinker.printNumberOfLines(output,numberedOutput);
+        //Для отчета
+        //String numberedInput = "RightBinaryToDecimal/NumberedBeforeLinkingEdsac.txt";
+        //String numberedOutput = "RightBinaryToDecimal/NumberedAfterLinkingEdsac.txt";
+        //edsacLinker.printNumberOfLines(input,numberedInput);
+        //edsacLinker.printNumberOfLines(output,numberedOutput);
 
 
     }
